1ee9917abc467aff06243272cf50ff73e8f89a40
Chandan Behera <chandanbehera4598@gmail.com>
2026-09-08 16:19:12 +0000

    if

---
+++ 03_control_flow/one.js
@@ -0,0 +1,21 @@
+//if
+
+// const isUserlogged=true
+// if(2==="2")
+// {
+// console.log("executed")
+// }
+
+// 2<=2
+
+// <, >, <=, >=, ==, !=, ===,!==
+
+// const score=200
+// if(score>100)
+// {
+//     const power="fly"
+//     console.log(`user power: ${power}`)
+// }else{
+
+// }
+
