8c85f276f8274c3a20e84e575fc36954173cf2d0
Chandan Behera <chandanbehera4598@gmail.com>
2026-09-08 16:45:30 +0000

    switch.js

---
+++ 03_control_flow/02_switch.js
@@ -0,0 +1,52 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month=3
+switch(month)
+{
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("mar");
+        break;
+    case 4:
+        console.log("apr");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("jun");
+        break;
+    case 7:
+        console.log("juny");
+        break;
+    case 8:
+        console.log("aug");
+        break;
+    case 9:
+        console.log("sep");
+        break;
+    case 10:
+        console.log("oct");
+        break;
+    case 11:
+        console.log("nuv");
+        break;
+    case 12:
+        console.log("dec");
+        break;
+        default:
+            console.log("invalid Data")
+            break;
+}
