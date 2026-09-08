4badf0b8f62cb7c251cebd65e0ddff69e01cfb18
Chandan Behera <chandanbehera4598@gmail.com>
2026-09-08 16:27:52 +0000

    one.js file add

--- 03_control_flow/one.js
+++ 03_control_flow/one.js
@@ -19,6 +19,31 @@
 
 // }
 
-const balance=1000
+const balance = 1000
 
-if(balance>500) console.log("test");
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if(balance<500)
+// {
+// console.log("less than 500")
+// }else if(balance<750)
+// {
+// console.log("less than 750")
+// }else{
+//     console.log("less than 1250")
+// }
+
+
+const userLoggedInn=true
+const debitCard=true
+const loggedInEmail=false
+const loggedInGoogle=true
+if(userLoggedInn && debitCard)
+{
+console.log("Allow to buy course")
+}
+
+if(loggedInEmail || loggedInGoogle)
+{
+    console.log("User logged in")
+}
